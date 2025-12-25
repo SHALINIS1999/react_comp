7cb6db27dd3646a2c3e8d1e0c99ad7328f1484f0
shalini <shalinisugumar49@gmail.com>
2025-12-25 13:49:24 +0530

    new ui page

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
     // <BrowserRouter>
     <div>
       {/* <Accord/> */}
-      {/* <ApiCall/> */}
+      <ApiCall/>
       {/* <ApiCallHooks/> */}
       {/* <ApiCallTwo/> */}
       {/* <Badge/> */}
